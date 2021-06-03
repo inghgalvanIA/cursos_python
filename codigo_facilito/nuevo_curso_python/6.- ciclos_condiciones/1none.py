f75fded876ad7cf57fe587e2d60f95392effac8a
Hector Galvan <inghgalvan@gmail.com>
2021-06-03 17:52:02 -0500

    inicio ciclos

---
+++ codigo_facilito/nuevo_curso_python/6.- ciclos_condiciones/1none.py
@@ -0,0 +1,15 @@
+"""
+None es un tipo de dato para representar la ausencia de un valor en  una variable
+muy usado cuando no sabemos que tipo de valor usaremos mas adelante
+
+"""
+
+resultado = None
+
+print(resultado)
+print(type(resultado))
+
+resultado = [1,2,3]
+
+print(resultado)
+print(type(resultado))
